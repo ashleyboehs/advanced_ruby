cec997ad1e8b902f1c8385b58737e981a0490afe
Ashley Boehs <ashley@boehs.com>
2024-01-19 12:04:33 -0600

    rails route mapper

--- dsl/dsl.rb
+++ dsl/dsl.rb
@@ -1,5 +1,5 @@
 class Configuration
-   def initialize(*attributes)      
+   def initialize(*attributes) 
      @config = {}
     
       attributes.each do |attribute|
@@ -14,6 +14,44 @@ class Configuration
    end
 end
 
+class RouteSet
+  def initialize
+    @routes = {}
+  end
+
+  def draw(&block)
+    mapper = RouteMapper.new(self)
+    mapper.instance_eval(&block)
+  end
+
+  def add_route(type, path)
+    @routes[path] = { type: type, action: "show" }
+  end
+
+  def lookup_action(path)
+    @routes[path][:action]
+  end
+end
+
+class RouteMapper
+  def initialize(route_set)
+    @route_set = route_set
+  end
+
+  def get(name)
+    @route_set.add_route("GET", name)
+  end
+
+  def post(name)
+    @route_set.add_route("POST", name)
+  end
+
+  def delete(name)
+    @route_set.add_route("DELETE", name)
+  end
+end
+
+  
 class Rails
   def self.configure(&block)
     instance_eval(&block)
@@ -21,6 +59,10 @@ class Rails
 
   def self.config
     @config ||= Configuration.new(:feature, :whatever)
+  end
+
+  def self.routes
+    @routes ||= RouteSet.new
   end
 end
 
@@ -32,11 +74,23 @@ Rails.configure do
   config.whatever = false
 end
 
-p Rails.config
-p Rails.config.feature
-p Rails.config
+#p Rails.config
+#p Rails.config.feature
+#p Rails.config
 
 config = Configuration.new(:foo)
 config.foo = "hello"
-p config.foo
-p config
+#p config.foo
+#p config
+
+Rails.routes.draw do
+  get :about
+  post :users
+  delete :users
+
+  p self
+end
+
+p Rails.routes
+p Rails.routes.lookup_action(:about)
+
